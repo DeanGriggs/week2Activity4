2cf8b2bb7291aa124d02e8111f0885da9bf62ef6
DeanGriggs <117603385+DeanGriggs@users.noreply.github.com>
2022-11-27 10:50:48 -0700

    Add files via upload

---
+++ calculation/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace calculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number of Seconds";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "That is:";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(235, 200);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(235, 12);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbNumber.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 251);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(800, 290);
+            this.MinimumSize = new System.Drawing.Size(800, 290);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ calculation/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+           String f = tbNumber.Text;
+            Console.WriteLine(f.GetType());
+            int i=Int32.Parse(f);
+            double ii=Double.Parse(f);
+            double decdec = 0;
+            double yryr = 0;
+            double dayday = 0;
+            double hrhr = 0;
+            double minmin = 0;
+            double secsec = 0;
+            int dec = 0;
+            int yr = 0;
+            int day = 0;
+            int hr = 0;
+            int min = 0;
+            int sec = 0;
+
+            if (i < 60)
+            {
+                label3.Text = (i) + " Seconds";
+            }
+            else if (i >= 60 && i < 3600)
+            {
+                min = i / 60;
+                sec = i % 60;
+                label3.Text = min + " Minutes" + " and " + sec + " Seconds";
+            }
+            else if (i >= 3600 && i < 86400)
+            {
+                hr = i / 3600;
+                min = i % 3600 / 60;
+                sec = i % 3600 % 60;
+                label3.Text = hr + " Hour[s]" + " and " + min + " Minute[s]" + " and " + sec + " Second[s]";
+            }
+            else if (i >= 86400 && i < 31536000)
+            {
+                
+                    day = i / 86400;
+                    hr = i % 86400 / 3600;
+                    min = i % 86400 % 3600 / 60;
+                    sec = i % 86400 % 3600 % 60;
+                    label3.Text = day + " Day[s]" + " and " + hr + " Hour[s]" + " and " + min + " Minute[s]" + " and " + sec + " Second[s]";
+            }
+            else if (i >= 31536000 && i < 315360000)
+            {
+                yr = i / 31536000;
+                day = i % 31536000 / 86400;
+                hr = i % 31536000 % 86400 / 3600;
+                min = i % 31536000 % 86400 % 3600 / 60;
+                sec = i % 31536000 % 86400 % 3600 % 60;
+                label3.Text = yr + " Year[s]" + " and " + day + " Day[s]" + " and " + hr + " Hour[s]" + " and " + min + " Minute[s]" + " and " + sec + " Second[s]";
+            }
+            else if (i >= 315360000 && ii < 3153600000)
+            {
+                decdec = ii / 315360000;
+                yryr = ii % 315360000 / 31536000;
+                dayday = ii % 315360000 % 31536000 / 86400;
+                hrhr = ii % 315360000 % 31536000 % 86400 / 3600;
+                minmin = ii % 315360000 % 31536000 % 86400 % 3600 / 60;
+                secsec = ii % 315360000 % 31536000 % 86400 % 3600 % 60;
+                dec = (int)decdec;
+                yr = (int)yryr;
+                day = (int)dayday;
+                hr = (int)hrhr;
+                min = (int)minmin;
+                sec = (int)secsec;
+
+
+                label3.Text = dec + " Decade[s]" + " and " + yr + " Year[s]" + " and " + day + " Day[s]" + " and " + hr + " Hour[s]" + " and " + min + " Minute[s]" + " and " + sec + " Second[s]";
+                   label4.Text =  " But for real no more. You're gonna break me.";
+            }
+            else
+            {
+                label3.Text = "error";
+            }
+            }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbNumber.Text = "";
+            label3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbNumber.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+        }
+    }
+    }
+
